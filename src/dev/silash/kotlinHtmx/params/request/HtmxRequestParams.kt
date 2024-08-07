4c9a0e39c78f6e3de99e84b1d17340a5533199ee
Silas <contact@silash.dev>
2024-08-07 18:44:12 +0200

    add htmx params

---
+++ src/dev/silash/kotlinHtmx/params/request/HtmxRequestParams.kt
@@ -0,0 +1,12 @@
+package dev.silash.kotlinHtmx.params.request
+
+object HtmxRequestParams {
+    const val BOOSTED = "HX-Boosted"
+    const val CURRENT_URL = "HX-Current-URL"
+    const val HISTORY_RESTORE_REQUEST = "HX-History-Restore-Request"
+    const val PROMPT = "HX-Prompt"
+    const val REQUEST = "HX-Request"
+    const val TARGET = "HX-Target"
+    const val TRIGGER_NAME = "HX-Trigger-Name"
+    const val TRIGGER = "HX-Trigger"
+}
